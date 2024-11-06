6ae41a0c653241aa76e687dc4e3005aae1e126c1
Catalyst6787 <hg9xvvkh@notinmyinbox.simplelogin.com>
2024-11-06 21:22:20 +0100

    simplified main reverted back lasta changes

--- gnl2/get_next_line.c
+++ gnl2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lfaure <lfaure@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:13:02 by lfaure            #+#    #+#             */
-/*   Updated: 2024/11/06 21:16:47 by lfaure           ###   ########.fr       */
+/*   Updated: 2024/11/06 21:20:44 by lfaure           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,14 +98,12 @@ char*	get_next_line(int fd)
 	char	*tmp;
 	
 	tmp = NULL;
-	if (get_stat(NULL, 2) && checkline(get_stat(NULL, 2)) == 2)
+	if (get_stat(NULL, 2) && checkline(get_stat(NULL, 2)))
 	{
 		tmp = until_nl(get_stat(NULL, 2), 0);
 		get_stat(after_nl(get_stat(NULL, 2)), 1);
 		return(tmp);
 	}
-	else if (get_stat(NULL, 2) && checkline(get_stat(NULL, 2)) == 1)
-		return(get_stat(NULL, 0), until_nl(get_stat(NULL, 2), 0));
 	else if (get_stat(NULL, 2))
 		tmp = ft_strjoin(get_stat(NULL, 2), "");
 	chars_read = read(fd, buf, BUFFER_SIZE);
@@ -123,57 +121,13 @@ char*	get_next_line(int fd)
 	return(get_stat(NULL, 3), tmp);
 }
 
-
+/*
 int main(void)
 {
 	int fd;
-	/*
-	printf("test checkline: \n");
-	printf("empty: %d\n", checkline(""));
-	printf("NULL: %d\n", checkline(NULL));
-	printf("nl with rest: %d\n", checkline("hello\n i am naughty"));
-	printf("nl no rest: %d\n", checkline("hello\n"));
-	printf("just nl: %d\n", checkline("\n"));
-	printf("multiple nl: %d\n", checkline("\n\n\n\n"));
-	printf("\n");
-	printf("test until nl: \n");
-	printf("empty: %s\n", until_nl("", 0));
-	printf("NULL: %s\n", until_nl(NULL, 0));
-	printf("nl with rest: %s\n", until_nl("hello\n i am naughty", 0));
-	printf("nl no rest: %s\n", until_nl("hello\n", 0));
-	printf("just nl: %s\n", until_nl("\n", 0));
-	printf("multiple nl: %s\n", until_nl("\n\n\n\n", 0));
-	printf("\n");
-	
-	printf("test after nl: \n");
-	printf("empty: %s\n", after_nl(""));
-	printf("NULL: %s\n", after_nl(NULL));
-	printf("nl with rest: %s\n", after_nl("hello\n i am naughty"));
-	printf("nl with rest containing nl: %s\n", after_nl("hello\n i am \n naughty \n naughty"));
-	printf("nl no rest: %s\n", after_nl("hello\n"));
-	printf("just nl: %s\n", after_nl("\n"));
-	printf("multiple nl: %s\n", after_nl("\n\n\n\n"));
-	
-	printf("\n");
-	printf("test get stat: \n");
-	printf("free: %s\n", get_stat(NULL, 0));
-	printf("assigne test: %s\n", get_stat(ft_strdup("test"), 1));
-	printf("get value: %s\n", get_stat(NULL, 2));
-	printf("free: %s\n", get_stat(NULL, 0));
-	printf("get value: %s\n", get_stat(NULL, 2));
-	*/
-
 	fd = open("./files/multiple_nl.txt", O_RDONLY);
 	
 	char* returnvalue;
-	/*
-	while((returnvalue = get_next_line(fd)) && returnvalue)
-	{
-		printf("%s", returnvalue);
-		free(returnvalue);
-		returnvalue = NULL;
-	}
-	*/
 
 	int i = 0;
 	char	*tmp;
@@ -193,22 +147,8 @@ int main(void)
 	}
 
 	printf("\n");
-	/*
-		
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	ft_strdup("");
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	*/	
+
 
 }
+*/
 
