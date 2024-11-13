3f6cdc24c20b55e86943a682d6c2213d5bdba388
Lucien Faure <lfaure@c1r5s2.42lausanne.ch>
2024-11-13 15:06:43 +0100

    sooolong

--- solong/main.c
+++ solong/main.c
@@ -1,56 +1,94 @@
-#include "./mlx/mlx.h"
-#include <stdlib.h>
-#include <unistd.h>
+#include "so_long.h"
 
-typedef struct s_data {
-    void    *img;
-    void    *nimg;
-    char    *addr;
-    char    *naddr;
-    int     bits_per_pixel;
-    int     line_length;
-    int     endian;
-    int     first_dots_shown;
-}               t_data;
-
-void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_data *d, int x, int y, int color)
 {
     char *dst;
+	if (x >= 0 && x <= d->win_l && y >= 0 && y <= d->win_h) // prevents putting pixel outside of window 
+	{
+		dst = d->img_addr + (y * d->line_length + x * (d->bits_per_pixel / 8)); // calculate offset in winlen
+    	*(unsigned int*)dst = color;
+	}
+}
+/*
+int move_little_pixel(t_data *d, int direction)
+{
+	static int y;
+	static int x;
 
-    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8)); // calculate offset in winlen
-    *(unsigned int*)dst = color;
+	if (!y)
+		y = 10;
+	if (!x)
+		x = 10;
+	mlx_put_image_to_window(d->mlx, d->win, d->img, 0, 0);
+	if (direction == 0)
+		my_mlx_pixel_put(d, x, y--, 0xf9f9f9);
+	else if (direction == 2)
+		my_mlx_pixel_put(d, x++, y, 0xf9f9f9);
+	else if (direction == 1)
+		my_mlx_pixel_put(d, x, y++, 0xf9f9f9);
+	else if (direction == 3)
+		my_mlx_pixel_put(d, x--, y, 0xf9f9f9);
+	return(0);
+}
+*/
+int	key_handler(int key, t_data *d)
+{
+	(void)d;
+	printf("EVENT! Key pressed: %d\n", key);
+	if (key == 53 || key == 12)
+	{
+		mlx_hook(d->win, 0, 0, close, &d);
+		return(end_program(d));
+	}
+	/*
+	if (key == 13)
+		move_little_pixel(d, 0);
+	else if (key == 0)
+		move_little_pixel(d, 3);
+	else if (key == 1)
+		move_little_pixel(d, 1);
+	else if (key == 2)
+		move_little_pixel(d, 2);
+	*/
+	return(1);
 }
 
-int main(void)
+//free everything here
+int end_program(t_data *d)
 {
-    void    *mlx;
-    void    *mlx_win;
-    t_data  img;
+	mlx_destroy_image(d->mlx, d->img);
+	mlx_destroy_window(d->mlx, d->win);
+	exit(0);
+}
 
-    mlx = mlx_init();
-    if (!mlx)
+int	main(void)
+{
+    t_data  d;
+
+	d.win_l = 1920;
+	d.win_h = 1080;
+    d.mlx = mlx_init();
+    if (!d.mlx)
         return (1);
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-    if (!mlx_win)
-        return (free(mlx), 1);
+    d.win = mlx_new_window(d.mlx, d.win_l, d.win_h, "Hello world!");
+    if (!d.win)
+        return (free(d.mlx), 1);
 
-    img.img = mlx_new_image(mlx, 1920, 1080);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-    img.first_dots_shown = 0;  // initialize to false
+    d.img = mlx_new_image(d.mlx, d.win_l, d.win_h);
+    d.img_addr = mlx_get_data_addr(d.img, &d.bits_per_pixel, &d.line_length, &d.endian);
 
-    // Initial dots
-    my_mlx_pixel_put(&img, 5, 5, 0xf9f9f9);
-    my_mlx_pixel_put(&img, 5, 7, 0xf9f9f9);
-    my_mlx_pixel_put(&img, 5, 9, 0xf9f9f9);
+    my_mlx_pixel_put(&d, 5, 5, 0xf9f9f9);
+    my_mlx_pixel_put(&d, 5, 7, 0xf9f9f9);
+    my_mlx_pixel_put(&d, 5, 9, 0xf9f9f9);
 
-    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    mlx_put_image_to_window(d.mlx, d.win, d.img, 0, 0);
 
-    // Start the event loop with the update function
-    //mlx_loop_hook(mlx, update_screen, &img);
+	mlx_hook(d.win, ON_DESTROY, 0, end_program, &d); // end program on window close
+	mlx_hook(d.win, 2, 0, key_handler, &d);
 
-    mlx_loop(mlx);
+    mlx_loop(d.mlx);
     
-    mlx_destroy_window(mlx, mlx_win);
-    free(mlx);
+    mlx_destroy_window(d.mlx, d.win);
+    free(d.mlx);
     return 0;
 }
--- solong/so_long.h
+++ solong/so_long.h
@@ -4,5 +4,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "mlx/mlx.h"
+
+# define ON_KEYDOWN 2
+# define ON_KEYUP 3
+# define ON_MOUSEDOWN 4
+# define ON_MOUSEUP 5
+# define ON_MOUSEMOVE 6
+# define ON_EXPOSE 12
+# define ON_DESTROY 17
+
+typedef struct s_data {
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*img_addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		win_l;
+	int		win_h;
+}	t_data;
+
+void    my_mlx_pixel_put(t_data *d, int x, int y, int color);
+int key_handler(int key, t_data *d);
+int end_program(t_data *d);
+int main(void);
 
 #endif
