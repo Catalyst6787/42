4b7fcc2d15711eafee35711901fcd0f28832d9be
Lucien Faure <lfaure@c2r9s9.42lausanne.ch>
2024-11-18 10:33:01 +0100

    moved struct t_data to heap

--- solong/main.c
+++ solong/main.c
@@ -6,7 +6,7 @@
 /*   By: lfaure <lfaure@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 10:19:56 by lfaure            #+#    #+#             */
-/*   Updated: 2024/11/18 10:20:12 by lfaure           ###   ########.fr       */
+/*   Updated: 2024/11/18 10:27:07 by lfaure           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,30 +57,30 @@ int	main(int argc, char **argv)
 {
 	if (argc < 2)
 		return(0);
-    t_data  d;
-	if(!init_struct(&d))
-		return(end_program(&d), 0);
-	if(!get_map(&d, argv[1]))
+    t_data  *d = malloc(sizeof(t_data));
+	if(!init_struct(d))
+		return(end_program(d), 0);
+	if(!get_map(d, argv[1]))
 		return(0);
-	d.win_h = d.map_h * d.asset_height;
-	d.win_l = d.map_l * d.asset_width;
-	if(!init_mlx(&d))
-		return(end_program(&d), 0);
+	d->win_h = d->map_h * d->asset_height;
+	d->win_l = d->map_l * d->asset_width;
+	if(!init_mlx(d))
+		return(end_program(d), 0);
 	
-	if(!init_assets(&d))
-		return(end_program(&d), 0);
+	if(!init_assets(d))
+		return(end_program(d), 0);
 	
-	print_map_debug(&d);
+	print_map_debug(d);
 
-	if (!check_map(&d))
-		return(end_program(&d), 0);
-	render_map(&d);
+	if (!check_map(d))
+		return(end_program(d), 0);
+	render_map(d);
 
-	mlx_hook(d.win, ON_DESTROY, 0, end_program, &d); // end program on window close
-	mlx_hook(d.win, 2, 0, key_handler, &d);
-	mlx_loop(d.mlx);
+	mlx_hook(d->win, ON_DESTROY, 0, end_program, d); // end program on window close
+	mlx_hook(d->win, 2, 0, key_handler, d);
+	mlx_loop(d->mlx);
 
-	mlx_destroy_window(d.mlx, d.win);
-	free(d.mlx);
+	mlx_destroy_window(d->mlx, d->win);
+	free(d->mlx);
 	return 0;
 }
