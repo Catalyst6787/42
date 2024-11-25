58cdd5036dca0d1c08abe7e632c9304ae9088e48
Catalyst6787 <hg9xvvkh@notinmyinbox.simplelogin.com>
2024-11-25 13:58:06 +0100

    copy list

--- pushswap/main.c
+++ pushswap/main.c
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lfaure <lfaure@student.42lausanne.ch>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/22 16:17:31 by lfaure            #+#    #+#             */
-/*   Updated: 2024/11/25 13:27:55 by lfaure           ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   main.c											 :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: lfaure <lfaure@student.42lausanne.ch>	  +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2024/11/22 16:17:31 by lfaure			#+#	#+#			 */
+/*   Updated: 2024/11/25 13:56:14 by lfaure		   ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "header.h"
@@ -35,24 +35,53 @@ t_stack	*lst_add_back(t_stack *list, t_stack *elem)
 	list->next = elem;
 	return (list);
 }
+
 void free_list(t_stack *list)
 {
-    t_stack *tmp;
-    while (list)
-    {
-        tmp = list;
-        list = list->next;
-        free(tmp);
-    }
+	t_stack *tmp;
+	while (list)
+	{
+		tmp = list;
+		list = list->next;
+		free(tmp);
+	}
 }
 void print_list(t_stack *list)
 {
-    while (list)
-    {
-        printf("%d -> ", list->nbr);
-        list = list->next;
-    }
-    printf("NULL\n");
+	while (list)
+	{
+		printf("%d -> ", list->nbr);
+		list = list->next;
+	}
+	printf("NULL\n");
+}
+
+t_stack *copy_list(t_stack *list)
+{
+	t_stack *copy = NULL;
+	t_stack *tail = NULL;
+
+	while (list)
+	{
+		t_stack *new_elem = lst_new(list->nbr);
+		if (!new_elem)
+		{
+			free_list(copy);
+			return NULL;
+		}
+		if (copy == NULL)
+		{
+			copy = new_elem;
+			tail = new_elem;
+		}
+		else
+		{
+			tail->next = new_elem;
+			tail = new_elem;
+		}
+		list = list->next;
+		}
+	return (copy);
 }
 
 int	main(int ac, char **av)
@@ -71,7 +100,12 @@ int	main(int ac, char **av)
 		lst_add_back(st_a, lst_new(ft_atoi(av[i])));
 		i++;
 	}
+	t_stack *copy = copy_list(st_a);
+	ft_printf("original list:\n");
 	print_list(st_a);
+	ft_printf("copy of list\n");
+	print_list(copy);
 	free_list(st_a);
+	free_list(copy);
 	return (1);
 }
