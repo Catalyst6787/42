858f3257e7ab6d740dc0626dd35a5eaf23f858ce
Lucien Faure <lfaure@c1r8s4.42lausanne.ch>
2024-12-13 16:48:36 +0100

    added time check for optimisation

--- pushswapv3/main.c
+++ pushswapv3/main.c
@@ -1,7 +1,12 @@
 #include "header.h"
+#include <time.h>
 
 int	main(int ac, char **av)
 {
+	time_t start,end; // TESTING
+	double dif; // TESTING
+	time (&start); // TESTING
+
 	int tmp;
 
 	if (ac < 2)
@@ -20,8 +25,22 @@ int	main(int ac, char **av)
 	if (tree->size_a < 2)
 		return(free_tree(&tree), ft_printf("only one element in list\n"), 0);
 
-	branch_out(&tree, 3);
+	branch_out(&tree, 5);
 
-	print_tree(&tree);
+	//print_tree(&tree);
 	free_tree(&tree);
+
+	time (&end); // TESTING
+	dif = difftime (end,start); // TESTING
+	printf ("\n\nYour calculations took %.2lf seconds to run.\n", dif ); // TESTING
+	
+	return (0);
 }
+/*
+time per lvl:
+
+<= 4 : 0s
+5 : 1s
+6 : 3s
+7 : 31s
+*/
