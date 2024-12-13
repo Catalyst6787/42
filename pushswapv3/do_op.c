09d23fa27c54188a70ec3108c6f3265bfb6c39b5
Lucien Faure <lfaure@c1r8s4.42lausanne.ch>
2024-12-13 16:19:24 +0100

    fixed error in do op

--- pushswapv3/do_op.c
+++ pushswapv3/do_op.c
@@ -8,18 +8,28 @@ void	do_op(t_tree *prev, t_tree **br, int op)
 	{
 		swap(&(prev->st_a), &(*br)->st_a, prev->size_a);
 		swap(&(prev->st_a_optid), &(*br)->st_a_optid, prev->size_a);
+		copy_st_malloc(prev->size_b, &prev->st_b, &(*br)->st_b);
+		copy_st_malloc(prev->size_b, &prev->st_b_optid, &(*br)->st_b_optid);
 		(*br)->size_a = prev->size_a;
+		(*br)->size_b = prev->size_b;
 	}
 	else if (op == SB)
 	{
 		swap(&(prev->st_b), &(*br)->st_b, prev->size_b);
 		swap(&(prev->st_b_optid), &(*br)->st_b_optid, prev->size_b);
+		copy_st_malloc(prev->size_a, &prev->st_a, &(*br)->st_a);
+		copy_st_malloc(prev->size_a, &prev->st_a_optid, &(*br)->st_a_optid);
 		(*br)->size_b = prev->size_b;
+		(*br)->size_a = prev->size_a;
 	}
 	else if (op == SS)
 	{
-		do_op(prev, br, SA);
-		do_op(prev, br, SB);
+		swap(&(prev->st_a), &(*br)->st_a, prev->size_a);
+		swap(&(prev->st_a_optid), &(*br)->st_a_optid, prev->size_a);
+		swap(&(prev->st_b), &(*br)->st_b, prev->size_b);
+		swap(&(prev->st_b_optid), &(*br)->st_b_optid, prev->size_b);
+		(*br)->size_b = prev->size_b;
+		(*br)->size_a = prev->size_a;
 	}
 	else
 		do_op2(prev, br, op);
@@ -41,13 +51,19 @@ void	do_op2(t_tree *prev, t_tree **br, int op)
 	{
 		rotate(&(prev->st_a), &(*br)->st_a, prev->size_a);
 		rotate(&(prev->st_a_optid), &(*br)->st_a_optid, prev->size_a);
+		copy_st_malloc(prev->size_b, &prev->st_b, &(*br)->st_b);
+		copy_st_malloc(prev->size_b, &prev->st_b_optid, &(*br)->st_b_optid);
 		(*br)->size_a = prev->size_a;
+		(*br)->size_b = prev->size_b;
 	}
 	else if (op == RB)
 	{
 		rotate(&(prev->st_b), &(*br)->st_b, prev->size_b);
 		rotate(&(prev->st_b_optid), &(*br)->st_b_optid, prev->size_b);
+		copy_st_malloc(prev->size_a, &prev->st_a, &(*br)->st_a);
+		copy_st_malloc(prev->size_a, &prev->st_a_optid, &(*br)->st_a_optid);
 		(*br)->size_b = prev->size_b;
+		(*br)->size_a = prev->size_a;
 	}
 	else
 		do_op3(prev, br, op);
@@ -57,24 +73,38 @@ void	do_op3(t_tree *prev, t_tree **br, int op)
 {
 	if (op == RR)
 	{
-		do_op2(prev, br, RA);
-		do_op2(prev, br, RB);
+		rotate(&(prev->st_a), &(*br)->st_a, prev->size_a);
+		rotate(&(prev->st_a_optid), &(*br)->st_a_optid, prev->size_a);
+		rotate(&(prev->st_b), &(*br)->st_b, prev->size_b);
+		rotate(&(prev->st_b_optid), &(*br)->st_b_optid, prev->size_b);
+		(*br)->size_b = prev->size_b;
+		(*br)->size_a = prev->size_a;
 	}
 	else if (op ==  RRA)
 	{
 		rev_rotate(&(prev->st_a), &(*br)->st_a, prev->size_a);
 		rev_rotate(&(prev->st_a_optid), &(*br)->st_a_optid, prev->size_a);
+		copy_st_malloc(prev->size_b, &prev->st_b, &(*br)->st_b);
+		copy_st_malloc(prev->size_b, &prev->st_b_optid, &(*br)->st_b_optid);
 		(*br)->size_a = prev->size_a;
+		(*br)->size_b = prev->size_b;
 	}
 	else if (op ==  RRB)
 	{
 		rev_rotate(&(prev->st_b), &(*br)->st_b, prev->size_b);
 		rev_rotate(&(prev->st_b_optid), &(*br)->st_b_optid, prev->size_b);
+		copy_st_malloc(prev->size_a, &prev->st_a, &(*br)->st_a);
+		copy_st_malloc(prev->size_a, &prev->st_a_optid, &(*br)->st_a_optid);
 		(*br)->size_b = prev->size_b;
+		(*br)->size_a = prev->size_a;
 	}
 	else if (op == RRR)
 	{
-		do_op3(prev, br, RRA);
-		do_op3(prev, br, RRB);
+		rev_rotate(&(prev->st_a), &(*br)->st_a, prev->size_a);
+		rev_rotate(&(prev->st_a_optid), &(*br)->st_a_optid, prev->size_a);
+		rev_rotate(&(prev->st_b), &(*br)->st_b, prev->size_b);
+		rev_rotate(&(prev->st_b_optid), &(*br)->st_b_optid, prev->size_b);
+		(*br)->size_b = prev->size_b;
+		(*br)->size_a = prev->size_a;
 	}
 }
