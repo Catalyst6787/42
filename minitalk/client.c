0b0dd4182330d8e481fd9ef3e1bf460a283d46bc
Lucien Faure <lfaure@c2r9s6.42lausanne.ch>
2024-12-21 18:49:31 +0100

    minitalk working

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,8 +1,41 @@
 #include "client.h"
 
+void	send_char(char c, int pid)
+{
+	int i;
+	int bit;
+
+	i = 7;
+	bit = 0;
+	while(i >= 0)
+	{
+		bit = (c >> i) & 1;
+		if (bit)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		i--;
+	}
+}
+
 int main(int ac, char **av)
 {
-	if (ac != 2)
-		return(0);
-	ft_printf("pid of server: %d", ft_atoi(av[1]));
+	int i;
+	int server_pid;
+
+	if (ac != 3)
+		return 0;
+
+	server_pid = ft_atoi(av[1]);
+	ft_printf("\nclient received pid: %d\n", server_pid);
+	i = 0;
+	while (av[2][i])
+	{
+		ft_printf("sending char> \"%c\"\n", av[2][i]);
+		send_char(av[2][i], server_pid);
+		i++;
+	}
+	ft_printf("\ndone! sent all chars.\n");
+	return(0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,6 +1,32 @@
 #include "server.h"
 
+void signal_handler(int signum)
+{
+	static char	res;
+	static int	bit_pos;
+	if (signum == SIGUSR1)
+		res |= (1 << (7 - bit_pos));
+	bit_pos++;
+	if (bit_pos == 8)
+	{
+		ft_printf("%c", res);
+		res = 0;
+		bit_pos = 0;
+	}
+}
+
 int main(void)
 {
-	ft_printf("%d\n", getpid());
+    struct sigaction action;
+    action.sa_handler = signal_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+
+    sigaction(SIGUSR1, &action, NULL);
+    sigaction(SIGUSR2, &action, NULL);
+
+    ft_printf("%d\n\n", getpid());
+    while (1)
+        usleep(1000);
+    return 0;
 }
